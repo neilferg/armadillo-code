c50076eeee790a56d072478d25ae2db9f982dfb5
conrad <conrad@localhost.localdomain>
2020-12-05 13:11:17 +1000

    expand sign() to handle nan

--- include/armadillo_bits/arma_cmath.hpp
+++ include/armadillo_bits/arma_cmath.hpp
@@ -358,7 +358,7 @@ arma_inline
 typename arma_real_only<eT>::result
 arma_sign(const eT x)
   {
-  return (x > eT(0)) ? eT(+1) : ( (x < eT(0)) ? eT(-1) : eT(0) );
+  return (x > eT(0)) ? eT(+1) : ( (x < eT(0)) ? eT(-1) : ((x == eT(0)) ? eT(0) : x) );
   }
 
 
